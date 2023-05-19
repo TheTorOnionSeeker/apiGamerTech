968f1f3bff3aa6fdebc8b7b965508015dcd3a10a
unknown <guillermooscarn@gmail.com>
2023-05-19 13:31:03 -0300

    agregue rutas y controllers para user

--- src/db.js
+++ src/db.js
@@ -30,7 +30,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Product } = sequelize.models;
+const { Product, User } = sequelize.models;
 // Aca vendrian las relaciones
 
 // Un usuario tiene unicamente un role y una rol le puede corresponder de 0 a muchos usuarios
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ const {Router}=require('express');
 const router=Router();
 
 const productRouter=require('./productRoutes')
+const usersRouter=require('./usersRoutes')
 
 router.use('/product',productRouter)
+router.use('/users',usersRouter)
 
 module.exports = router;
