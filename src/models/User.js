60da2845317d899c67d2cb91221a26c7b62653fb
unknown <guillermooscarn@gmail.com>
2023-05-21 17:45:30 -0300

    agregué cambios y ruta para crear y verificar usuario

--- src/models/User.js
+++ src/models/User.js
@@ -12,8 +12,13 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     password: {
       type: DataTypes.STRING,
+      allowNull: false,
     }
   });
 };
