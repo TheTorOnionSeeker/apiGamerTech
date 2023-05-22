e685d4eb4f62d4041cea63ae23a38cc79feee9f9
Salvador Hilares <salvador.hilares@utec.edu.pe>
2023-05-22 18:55:55 -0500

    ordenamientos

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,10 +1,11 @@
 const { Router }=require('express');
 const router=Router();
-const {getAllProducts, getProductById, getProductByName, createProduct, modifyProduct}=require('../controllers/product.controllers.js');
+const {getAllProducts, getProductById, getProductByName, createProduct, modifyProduct, sortProducts}=require('../controllers/product.controllers.js');
 
 router.get('/',getAllProducts,getProductByName)
 router.get('/:id',getProductById)
-//router.get('/:name', getProductByName)
+router.post('/sort',sortProducts)
+router.get('/:name', getProductByName)
 router.post('/new',createProduct)
 router.post('/modifyproduct',modifyProduct)
 
