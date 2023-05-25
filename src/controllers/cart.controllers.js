7bc32acc7caa6d39c551c5475249f1db94fc14b7
unknown <guillermooscarn@gmail.com>
2023-05-24 23:14:39 -0300

    cambios

---
+++ src/controllers/cart.controllers.js
@@ -0,0 +1,42 @@
+const { Cart, User } = require("../db.js");
+
+async function createCart(req, res) {
+    const { productsId, userId } = req.body;
+    try {
+      const new_cart = await Cart.create({
+        productsId: productsId,
+      });
+      if (!new_cart) throw new Error("No se pudo crear el carrito");
+      const user = await User.findOne({
+        where: {
+          id: userId,
+        },
+      });
+      if (user !== null) await new_cart.setUser(user);
+      if (user === null) throw new Error("Usuario no encontrado!");
+      res.status(201).json({ cart: new_cart, msg: "Carrito creado" });
+    } catch (error) {
+      res.status(404).json({ error: error.message });
+    }
+}
+
+async function getCartById(req, res) {
+    const { id } = req.params;
+    try {
+      const cart = await Cart.findOne({
+        where: {
+          id: id,
+        },
+        attributes: ["productsId","userId"],
+      });
+      if (cart === null) throw new Error("Cart not found!");
+      res.status(200).json(cart);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  }
+
+module.exports = {
+    createCart,
+    getCartById
+  };
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,10 @@ const router=Router();
 
 const productRouter=require('./productRoutes')
 const usersRouter=require('./usersRoutes')
+const cartRouter=require('./cartRoutes')
 
 router.use('/product',productRouter)
 router.use('/users',usersRouter)
+router.use('/cart',cartRouter)
 
 module.exports = router;
