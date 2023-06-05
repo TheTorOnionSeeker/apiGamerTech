85972ebb447a0b2dc156f0b361cbe10b7e475480
Ema Schönborn <schonborn13@gmail.com>
2023-06-04 23:29:09 -0300

    modificación getAllUsers

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,7 +3,7 @@ const { User } = require("../db.js");
 async function getAllUsers(req, res) {
   try {
     const DBusers = await User.findAll({
-      attributes: ["id", "name", "email", "isActive"],
+      attributes: ["id", "name", "email", "isActive", "createdAt"],
     });
     if (DBusers === null) throw new Error("Usuarios no encontrados!");
     res.status(200).json(DBusers);
