4564226e16af335513a416056200c61ae603679a
Ema Schönborn <schonborn13@gmail.com>
2023-06-14 17:19:56 -0300

    se agrega atributo a getAllProducts

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -30,6 +30,7 @@ async function getAllProducts(req, res) {
         "imageUrl",
         "isActive",
         "stock",
+        "category",
         "reviewsScores",
         "reviewsTexts",
       ],
